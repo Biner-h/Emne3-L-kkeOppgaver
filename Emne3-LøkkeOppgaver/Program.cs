1128a494c8ea68c8351a7cb164c2d870429b7219
Biner Habib <Binerhabib@gmail.com>
2023-11-20 14:06:13 +0100

    Add project files.

---
+++ Emne3-LøkkeOppgaver/Program.cs
@@ -0,0 +1,46 @@
+﻿//Oppgave 1
+Console.WriteLine("Oppgave 1:");
+void forLoop()
+{
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine("Terje er kul");
+    }
+}
+forLoop();
+
+
+Console.WriteLine();
+
+
+//Oppgave 2
+Console.WriteLine("Oppgave 2:");
+void forEachLoop()
+{
+    string array = "Hei på deg";
+
+    foreach (char letter in array)
+    {
+        Console.WriteLine(letter);
+    }
+}
+forEachLoop();
+
+
+Console.WriteLine();
+
+
+//Oppgave 3
+Console.WriteLine("Oppgave 3:");
+void whileLoop()
+{
+    int i = 0;
+
+    while (i < 10)
+    {
+        Console.WriteLine("Runde nr: " + i);
+        i++;
+    }
+}
+whileLoop();
